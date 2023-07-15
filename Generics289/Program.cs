f1f537d12335ebe023ceb0691bab1d0afae463fd
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-15 10:47:33 +0530

    [Sachin] Adding Welcome Message

---
+++ Generics289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generics289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the generics program");
+        }
+    }
+}
