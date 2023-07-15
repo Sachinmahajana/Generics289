cf3cc09e4c5fbb4dc91994d3ba0fe565a8e9f920
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-15 10:51:58 +0530

    [Sachin] Adding UC1-FindMaxint

---
+++ Generics289/FindMaxint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics289
+{
+    internal class FindMaxint
+    {
+        public static void FindMaxintFunction(int num1, int num2, int num3)
+        {
+            // 50 20 30 (1,-1,0)
+            if (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0)
+            {
+                Console.WriteLine("{0} is greater number than {1} {2}", num1, num2, num3);
+            }
+            else if (num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) >= 0)
+            {
+                Console.WriteLine("{1} is a greater number", num2);
+            }
+            else
+            {
+                Console.WriteLine("{2} is a greater number");
+            }
+        }
+    }
+}
--- Generics289/Program.cs
+++ Generics289/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the generics program");
+            Console.WriteLine("Select the program given below");
+            Console.WriteLine("\n1:Findmaxint");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FindMaxint.FindMaxintFunction(50, 20, 30);
+                    break;
+            }
         }
     }
 }
