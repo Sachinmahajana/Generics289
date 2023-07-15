0e60d9ae82091db1279d9740d3a2a17b9b95677f
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-15 10:56:59 +0530

    [Sachin] Adding UC2-FindMaxminFloat

---
+++ Generics289/FindMaxminFloat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics289
+{
+    internal class FindMaxminFloat
+    {
+        public static void FindMaxMinFloatFunction(float first, float second, float third)
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{0} is a greater number than {1} {2}", first, second, third);
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine("{1} is greater than {0} {2}", second, first, third);
+            }
+            else
+            {
+                Console.WriteLine("{2} is greater than {0} {1}", third, first, second);
+            }
+        }
+    }
+}
--- Generics289/Program.cs
+++ Generics289/Program.cs
@@ -13,6 +13,9 @@
                 case 1:
                     FindMaxint.FindMaxintFunction(50, 20, 30);
                     break;
+                case 2:
+                    FindMaxminFloat.FindMaxMinFloatFunction(50.3f, 40.2f, 30.4f);
+                    break;
             }
         }
     }
